ec70def8ad69bb89b715a2e73065a9f754542ab7
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-21 11:55:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+// Function to highlight bold words in green
 function highlight() {
-    //Write your code here
-
-
+    const boldElements = document.querySelectorAll('strong');
+    for (const element of boldElements) {
+        element.style.color = 'green';
+    }
 }
 
-
+// Function to return the normal black color
 function return_normal() {
-    //Write your code here
-
-    
+    const boldElements = document.querySelectorAll('strong');
+    for (const element of boldElements) {
+        element.style.color = 'black';
+    }
 }
+
+// Attach event listeners to the link for hover events
+const link = document.getElementById('highlightLink');
+link.addEventListener('mouseover', highlight);
+link.addEventListener('mouseout', return_normal);
